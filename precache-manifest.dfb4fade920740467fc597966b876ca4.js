c392e4c6d7880af43fa67f9a934497192d97ea94
sibiyan <sibiyan9030@gmail.com>
2020-12-22 00:05:43 +0530

    Updates

---
+++ precache-manifest.dfb4fade920740467fc597966b876ca4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1780c4588cc5c29da9c2de288d2a8f87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde5efb576d27c9fcd45",
+    "url": "/static/js/2.ec02cb10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ec02cb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272a1f77eb973cc26f9a",
+    "url": "/static/js/main.83f17500.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "a65287f04b7caf76381519707f0045e8",
+    "url": "/static/media/profile_3.a65287f0.jpg"
+  },
+  {
+    "revision": "729a19b39bbee1e843d3c7450f9d8e9c",
+    "url": "/static/media/svg-14.729a19b3.svg"
+  },
+  {
+    "revision": "5118199275350eb1346478e5b3039e0f",
+    "url": "/static/media/svg-15.51181992.svg"
+  },
+  {
+    "revision": "0b3df8a7073e7236c2d6b4e629397700",
+    "url": "/static/media/svg-16.0b3df8a7.svg"
+  }
+]);
